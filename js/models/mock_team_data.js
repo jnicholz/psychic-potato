bc9f2737336821b3d73b1fe70640f4f7c8444f65
Jeremiah NIcholls <10808080@my.uvu.edu>
2022-08-06 15:02:53 -0600

    Changes to be committed: 	new file:   .gitignore 	new file:   css/bentMetal.jpg 	new file:   css/main.css 	new file:   images/UnBended.JPG 	new file:   images/benBev.jpg 	new file:   images/benderBending.jpg 	new file:   images/bent.jpg 	new file:   images/buster.jpg 	new file:   images/coilette.jpg 	new file:   images/firstBorn.jpg 	new file:   images/flexo.jpg 	new file:   images/muttNChop.jpg 	new file:   images/turner.jpg 	new file:   images/twoStripe.jpg 	new file:   images/vladimir.jpg 	new file:   images/woodenbender.jpg 	new file:   index.html 	new file:   js/app.js 	new file:   js/controllers/app_controller.js 	new file:   js/models/app_view_model.js 	new file:   js/models/local_storage_service.js 	new file:   js/models/mock_team_data.js 	new file:   js/models/team_view_model.js 	new file:   js/views/list_page_view.js 	new file:   js/views/partials/example_output.html 	new file:   js/views/partials/list_page_wrapper.ejs 	new file:   js/views/partials/list_view.ejs

---
+++ js/models/mock_team_data.js
@@ -0,0 +1,88 @@
+//MOCK TEAM DATA-This should reflect your league theme.  Feel free to customize for your needs
+ //note, I use the list.nameCol in the view model to refer to this column to display team names generically.
+  //note, I use the list.logoCol in the view model to refer to this column to display team logos generically (if used)
+
+let mockTeamData=[
+  {
+    "id": 0,
+    "name":"Ben Bev",       
+    "coachName": "Slurms MacKenzie",
+    "coachPhone": "(758)767-0622",
+    "numPlayers": 3,
+    "teamPhoto": "images/benBev.jpg"     
+  },
+  {
+    id: 1,
+    "name":"Bender Bending",
+    "coachName": "Philip Fry",
+    "coachPhone": "(744)547-0379",
+    "numPlayers": 6,
+    "teamPhoto": "images/benderBending.jpg"
+  },
+  {
+    id: 2,
+    "name":"Buster",
+    "coachName": "Rita Rodríguez",
+    "coachPhone": "(748)207-6374",
+    "numPlayers": 7,
+    "teamPhoto": "images/buster.jpg"
+  },
+  {
+    id: 3,
+    "name":"Coilette",
+    "coachName": "Prof. Farnsworth",
+    "coachPhone": "(776)303-2767",
+    "numPlayers": 8,
+    "teamPhoto": "images/coilette.jpg"
+  },
+  {
+    id: 4,
+    "name":"First Born",
+    "coachName": "Robot Devil",
+    "coachPhone": "(101)001-1010",
+    "numPlayers": 2,
+    "teamPhoto": "images/firstBorn.jpg"
+  },
+  {
+    id: 5,
+    "name":"Flexo",
+    "coachName": "Angleyne",
+    "coachPhone": "(718)264-5396",
+    "numPlayers": 3,
+    "teamPhoto": "images/flexo.jpg"
+  },
+  {
+    id: 6,
+    "name":"Mutt N Chop",
+    "coachName": "Mecha Hermes",
+    "coachPhone": "(632)420-4376",
+    "numPlayers": 7,
+    "teamPhoto": "images/muttNChop.jpg"
+  },
+  {
+    id: 7,
+    "name":"Turner",
+    "coachName": "Lord Nibbler",
+    "coachPhone": "(567)306-4225",
+    "numPlayers": 5,
+    "teamPhoto": "images/turner.jpg"
+  },
+  {
+    id: 8,
+    "name":"Two Stripe",
+    "coachName": "Zapp Brannigan",
+    "coachPhone": "(927)702-7266",
+    "numPlayers": 6,
+    "teamPhoto": "images/twoStripe.jpg"
+  },
+  {
+    id: 9,
+    "name":"Vladimir",
+    "coachName": "Dr. Zoidberg",
+    "coachPhone": "(370)964-3237",
+    "numPlayers": 4,
+    "teamPhoto": "images/vladimir.jpg"
+  }    
+]
+
+           
